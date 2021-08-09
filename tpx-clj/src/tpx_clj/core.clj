307ebffca42012df1e71373bc7d909adeddc2bcc
magnus bareid <magnus.bareid@inonit.no>
2021-08-09 13:28:34 +0200

    outlined first draft on clojurian tpx  mechanics.

--- tpx-clj/src/tpx_clj/core.clj
+++ tpx-clj/src/tpx_clj/core.clj
@@ -8,20 +8,51 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (init-ipc-with-cs7)
+  (retrieve-tpID))
+
+(defn watch-agent-func [_agent context]
+  "Boilerplate for making a watch-agent
+   Intended for async IPC with CS7, e.g. if pure stdin/out is used for this purpose"
+  (let [watch-fn (fn [_context _key _ref old-value new-value])]
+  (add-watch _agent nil (partial watch-fn context))))
+
+(defn init-ipc-with-cs7
+  "Initializes Inter-Process Communication with CS7.
+   Currently not complete; the methods for IPC have not been decided yet."
+  (println "Facilitating IPC with CS7...")
+  (println "Connection with CS7 established! (just kidding! still need to complete that thing)"))
+
+(defn retrieve-tpID
+  "Contains the instructions sent to CS7 to retrieve the tpID."
+  (println "Querying CS7 for tpID.")
+  (let [tpID "world"]
+       (println "Response from CS7 reads:" tpID)))
+
+
+(defn musician-specific-volume-control
+  "Relays musician-specific volume control towards CS7.
+   Takes musician's ID, and positive or negative integers.
+   "
+  [musician adjusted-amount]
+  (println "Asking CS7 politely to adjust volume of '"musician"' by: "adjusted-amount)
+  (println "CS7 politely agreed. Volume of '"musician"' adjusted by: "adjusted-amount))
+
+(defn unit-volume-control
+  "Relays volume control towards CS7
+   Takes positive or negative integers"
+  [adjusted-amount]
+  (println "Asking CS7 politely to adjust the unit's output volume by: " adjusted-amount)
+  (println "CS7 politely agreed. Unit's volume adjusted by:" adjusted-amount))
+
+
 
 (comment test area
-
          (connect.client/init {:nickname "heyho!"})
          (let [conn-map (mqtt-connection/init "my-topic")
-               handler-map {:a #(println (apply - %&))}]
+               handler-map {:volume_control #(println (apply - %&))}]
            
            (mqtt-connection/subscribe conn-map handler-map)
            
-           (mqtt-connection/publish conn-map [[:a][1 2 3]])
-           (mqtt-connection/publish conn-map [[:a] [3 3 3]])
-           (mqtt-connection/publish conn-map [[:a] [33 33 33]])
-           (mqtt-connection/publish conn-map [[:a] [-1 2 3]])
-           (mqtt-connection/publish conn-map [[:a] [0 0 0]])
-           (mqtt-connection/publish conn-map [[:a] [5 -1 -4]])
-           (mqtt-connection/publish conn-map [[:a] [33 -22 5]])))
+           (mqtt-connection/publish conn-map [[:a] [1 2 3]])
