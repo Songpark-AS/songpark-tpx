2ba1a52f9c46d8118ab14efdd10b27be10d46998
Achim Munene <achim_munene@hotmail.com>
2021-08-12 14:59:21 +0200

    WIP

--- tpx-clj/src/tpx_clj/core.clj
+++ tpx-clj/src/tpx_clj/core.clj
@@ -1,7 +1,12 @@
 (ns tpx-clj.core
-  (:gen-class))
+  (:gen-class)
+  (use '[clojure.java.shell :only [sh]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn talk-to-cs7 [name]
+  (let [[exit out err](sh "docker" "run" name)]
+    (println out)))
