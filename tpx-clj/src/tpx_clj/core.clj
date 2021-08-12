493c4748c82d6611c150ea9f53d8515aa7c048e9
magnus bareid <magnus.bareid@inonit.no>
2021-08-12 15:40:21 +0200

    refactored handler-map and its associated functions, then retested to ensure it still works as intended

--- tpx-clj/src/tpx_clj/core.clj
+++ tpx-clj/src/tpx_clj/core.clj
@@ -9,7 +9,7 @@
 
 (defn -main
   "Prepares mqtt connection"
-  [& args]
+  []
   ; (println "Hello, World!")
   ; (init-ipc-with-cs7)
   ; (retrieve-tpID))
@@ -17,9 +17,10 @@
 
 ;! Possibly useful later
 
-(defn watch-agent-func [_agent context]
-  "Boilerplate for making a watch-agent
+(defn watch-agent-func 
+  "Boilerplate for making a watch-agent.
    Intended for async IPC with CS7, e.g. if pure stdin/out is used for this purpose"
+  [_agent context]
   (let [watch-fn (fn [_context _key _ref old-value new-value])]
     (add-watch _agent nil (partial watch-fn context))))
 
@@ -28,46 +29,82 @@
 (defn init-ipc-with-cs7
   "Initializes Inter-Process Communication with CS7.
    Currently not complete; the methods for IPC have not been decided yet."
-  [& args] ; it did not want to evaluate without this definition?
+  [] ; it did not want to evaluate without this definition?
   (println "Facilitating IPC with CS7...")
   (println "Connection with CS7 established! (just kidding! still need to complete that thing)"))
 
 (defn retrieve-tpID
   "Contains the instructions sent to CS7 to retrieve the tpID."
-  [& args]
+  []
   (println "Querying CS7 for tpID.")
-  (let [tpID "world"]
+  (let [tpID "0000"] ; hardcoded to "0000" - used to say World, previously...
     (println "Response from CS7 reads: "tpID)
     tpID))
 
-(defn musician-specific-volume-control
+(defn adjust-volume-musician
   "Relays musician-specific volume control towards CS7.
    Takes musician's ID, and positive or negative integers.
-   "
+   Assumes CS7 has some kind of logic to deal with 'invalid values'."
   [musician adjusted-amount]
   (println "Asking CS7 politely to adjust volume of '"musician"' by: "adjusted-amount)
   (println "CS7 politely agreed. Volume of '"musician"' adjusted by: "adjusted-amount))
 
-(defn unit-volume-control
-  "Relays volume control towards CS7
-   Takes positive or negative integers"
+(defn adjust-volume-unit
+  "Relays volume control towards CS7.
+   Takes positive or negative integers.
+   Assumes CS7 has some kind of logic to deal with 'invalid values'."
   [adjusted-amount]
   (println "Asking CS7 politely to adjust the unit's output volume by: "adjusted-amount)
   (println "CS7 politely agreed. Unit's volume adjusted by: "adjusted-amount))
+
+(defn toggle-mute-musician
+  "Relays to CS7 instructions to toggle mute specific musicians through 2 steps.
+   The first step is possibly reduntant, 
+   but can be nice to have for ensuring correct UI representation on the mobile app.
+   1. Asks CS7 if the specific musician is muted or not already.
+   2. Sends instruction to CS7 to mute or unmute."
+  [musician]
+  (println "Checking current mute state of" musician)
+  (let ; Contact CS7 here.
+   [muted-or-not (rand-int[2])]
+    (println "The mute state of" musician "is" muted-or-not)
+    (println "Asking CS7 politely to mute/unmute" musician)
+    (println "CS7 politely toggled muted/unmute of" musician "to" muted-or-not)))
+
+(defn toggle-mute-unit
+  "Relays to CS7 instructions to mute specific musicians.
+   The first step is possibly reduntant, 
+   but can be nice to have for ensuring correct UI representation on the mobile app.
+   1. Asks CS7 if the unit is muted or not already.
+   2. Sends instruction to CS7 to mute or unmute."
+  []
+  (println "Checking current mute state of unit")
+  (let ; Contact CS7 here.
+   [muted-or-not (rand-int[2])]
+    (println "The mute state of unit is" muted-or-not)
+    (println "Asking CS7 politely to mute/unmute the unit")
+    (println "CS7 politely toggled the teleport mute/unmute to" muted-or-not)))
 
 ;! REPL execution playground
 
 ;? Imitating communication between phone and TPX unit
 
 ;; (def handler-map {:device {:volume-change #(println "Volume changed by " % " amount")}})
-(def handler-map {:device {:volume-change musician-specific-volume-control}})
+(def handler-map {:tpx-unit   { :adjust-volume-musician adjust-volume-musician
+                                :adjust-volume-unit     adjust-volume-unit
+                                :toggle-mute-musician   toggle-mute-musician
+                                :toggle-mute-unit       toggle-mute-unit}
+                  :phone-app  { :adjust-volume-musician adjust-volume-musician
+                                :adjust-volume-unit     adjust-volume-unit
+                                :toggle-mute-musician   toggle-mute-musician
+                                :toggle-mute-unit       toggle-mute-unit}})
 
 (defn initiate-communications
-  "Initiates communcations with the backend,
+  "Initiates communications with the backend,
    telling the backend its tpID,
    then initiates communications to MQTT's pub/sub"
-  [& args]
-  (let [tpid "0000"
+  []
+  (let [tpid (retrieve-tpID)
         plat-response (connect.tp/init {:tpid tpid})
         uuid (:uuid plat-response)
         status (:status plat-response)]
@@ -79,24 +116,24 @@
     (println "This is uuid: " uuid)))
 
 (defn fake-phone-commands
-  "Pretends to be a phone that hooks into the background,
+  "Pretends to be a phone that hooks into the backend,
    associates itself with a specific unit's tpID,
    then queries TPX via MQTT to interact with CS7."
-  [& args]
-  (let [nickname "christians.dream"
+  []
+  (let [;nickname "christians.dream"
         plat-response (connect.client/init {:nickname "christians.dream"})
         uuid (:uuid plat-response)
         status (:status plat-response)]
 
     (if (and status uuid)
       (let [conn-map (mqtt-connection/init uuid)]
-        (mqtt-connection/publish conn-map [[:device :volume-change] ["self" 123]])
-        (mqtt-connection/publish conn-map [[:device :volume-change] ["self" 123]])
-        (mqtt-connection/publish conn-map [[:device :volume-change] ["self" (rand)]])
-        (mqtt-connection/publish conn-map [[:device :volume-change] ["self" (rand)]])
-        (mqtt-connection/publish conn-map [[:device :volume-change] ["self" (rand)]])
-        (mqtt-connection/publish conn-map [[:device :volume-change] ["self" (rand)]])
-        (mqtt-connection/publish conn-map [[:device :volume-change] ["self" 321]])
+        (mqtt-connection/publish conn-map [[:tpx-unit :adjust-volume-musician] ["self" 123]])
+        (mqtt-connection/publish conn-map [[:tpx-unit :adjust-volume-musician] ["self" 123]])
+        (mqtt-connection/publish conn-map [[:tpx-unit :adjust-volume-musician] ["self" (rand)]])
+        (mqtt-connection/publish conn-map [[:tpx-unit :adjust-volume-musician] ["self" (rand)]])
+        (mqtt-connection/publish conn-map [[:tpx-unit :adjust-volume-musician] ["self" (rand)]])
+        (mqtt-connection/publish conn-map [[:tpx-unit :adjust-volume-musician] ["self" (rand)]])
+        (mqtt-connection/publish conn-map [[:tpx-unit :adjust-volume-musician] ["self" 321]])
         (println "Client publishes finished"))
       (println "Client connection to platform, failed"))))
 
