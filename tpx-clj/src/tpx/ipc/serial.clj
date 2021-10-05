5ee4f9d8f0d21fb1e16e61fcd7f4f69e3f8850e8
Daniel W. D <daniel.delatre@inonit.no>
2021-10-05 17:24:04 +0200

    Add basic serial communication functionality

---
+++ tpx-clj/src/tpx/ipc/serial.clj
@@ -0,0 +1,67 @@
+(ns tpx.ipc.serial
+  (:require [taoensso.timbre :as log]
+            [serial.core :as serial]
+            [clojure.java.io :as io]))
+
+(def commands
+  #{:volume/g :volume/l :volume/r
+    :filer/b :filter/l :filter/h})
+
+
+(defonce config (atom {:pts "/dev/pts/1"}))
+
+(defn process [s]
+  (future (log/debug ::process (read-string s))))
+
+(defn handler [io-stream]
+  "Read input stream from serial"
+  (with-open [reader (io/reader io-stream)]
+    (if-let [line (.readLine reader)]
+      (try
+        (process line)
+        (catch Exception e
+          (log/warn (ex-message e)))
+        (finally
+          (.close reader))))))
+
+
+(defn connect-to-port [pts]
+  (log/debug ::connect-to-port "connecting...")
+  (let [port (serial/open pts)]
+    (swap! config assoc :port port)
+    (serial/listen! port handler false)))
+
+
+(defn disconnect [port]
+  (log/debug ::disconnect "bye...")
+  (try
+    (serial/unlisten! port)
+    (serial/close! port)
+    (swap! config dissoc :port)
+    (catch Exception e
+      (log/error ::disconnect (ex-message))
+      (log/error ::disconnect (ex-data)))))
+
+
+(defn- str->ba [s]
+  (->> s
+       (map (comp byte int))
+       byte-array))
+
+
+(defn send-command [port cmd v]
+  (serial/write port (str->ba (str cmd " " v "\n"))))
+
+
+(comment
+
+  (log/debug @config)  
+  
+  (connect-to-port (:pts @config))
+
+  (send-command (:port @config) "vol" 100)
+
+  (disconnect (:port @config))  
+
+  )
+
