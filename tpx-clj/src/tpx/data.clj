7e057fdcb9af7f4c4e3e9819551c86caecbbad4c
Emil Bengtsson <emil@emil0r.com>
2021-10-20 00:02:42 +0200

    Add data holder
    Really quick and dirty way of dealing with global state.

---
+++ tpx-clj/src/tpx/data.clj
@@ -0,0 +1,28 @@
+(ns tpx.data)
+
+(defonce ^:private jam-id* (atom nil))
+(defonce ^:private tp-id* (atom nil))
+
+;; Jam ID (UUID)
+(defn set-jam-id! [jam-id]
+  (reset! jam-id* jam-id))
+
+(defn clear-jam-id! []
+  (reset! jam-id* nil))
+
+(defn get-jam-teleporters []
+  (str @jam-id* "/teleporters"))
+
+(defn get-jam []
+  (str @jam-id* "/jam"))
+
+
+;; TP ID (UUID)
+(defn set-tp-id! [tp-id]
+  (reset! tp-id* tp-id))
+
+(defn clear-tp-id! []
+  (reset! tp-id* nil))
+
+(defn get-tp-id []
+  (str @tp-id*))
