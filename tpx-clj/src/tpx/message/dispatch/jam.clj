ef3aecbea5a390b5c66d6d40e5be0aa83a247990
Emil Bengtsson <emil@emil0r.com>
2021-10-21 23:23:04 +0200

    Update dispatch jam to delay unsubscribing from the jam topic
    Currently at five seconds. The main problem is that we cannot close
    the the jam, until the HW has ended the stream. This is hairy
    territory, with lots of stuff that can go wrong. For now we just add
    the timeout.

--- tpx-clj/src/tpx/message/dispatch/jam.clj
+++ tpx-clj/src/tpx/message/dispatch/jam.clj
@@ -13,12 +13,48 @@
     (log/debug (str "Subscribing to " (data/get-jam-teleporters)))
     (send-message! {:message/type :platform.cmd/subscribe
                     :message/meta {:mqtt/topics {(data/get-jam-teleporters) 0}}})
+    (data/set-jam-session! body)
     (ipc.command/jam-start (:jam/members body) (:jam/sip body))))
 
 (defmethod message/dispatch :jam.cmd/stop [{:message/keys [body]
                                             :keys [mqtt-manager]}]
   (ipc.command/jam-stop (:jam/members body) (:jam/sip body))
-  (log/debug :dispatch (str "Unsubscribing from " (data/get-jam-teleporters)))
-  (.unsubscribe mqtt-manager [(data/get-jam-teleporters)])
-  (data/clear-jam-id!))
+  
+  (future
+    ;; sleep for 5 seconds, then close everything
+    ;; quick and dirty
+    (try
+      (log/debug "Sleeping for 5 seconds, before unsubscribing from jam-teleporters channel and clearing jam-id and jam-session")
+      (Thread/sleep (* 5 1000))
+      (log/debug :dispatch (str "Unsubscribing from " (data/get-jam-teleporters)))
+      (.unsubscribe mqtt-manager [(data/get-jam-teleporters)])
+      (.publish mqtt-manager (data/get-jam) {:message/type :jam.cmd/ended
+                                             :message/body {:teleporter/id (data/get-tp-id)
+                                                            :jam/uuid (data/get-jam-id)}})
+      (data/clear-jam-id!)
+      (data/clear-jam-session!)
+      (catch Throwable t
+        (log/error "Caught " (str t))))))
 
+
+#_(let [mqtt-manager (:mqtt-manager @tpx.init/system)]
+
+  #_(.publish mqtt-manager (data/get-jam) {:message/type :jam.cmd/ended
+                                         :message/body {:teleporter/id (data/get-tp-id)
+                                                        :jam/uuid (data/get-jam-id)}})
+  #_(.publish mqtt-manager (data/get-jam) {:message/type :testus
+                                         :message/body {:teleporter/id (data/get-tp-id)
+                                                        :jam/uuid (data/get-jam-id)}})
+  (log/debug "Sleeping for 5 seconds, before unsubscribing from jam-teleporters channel and clearing jam-id and jam-session")
+  ;;(Thread/sleep (* 5 1000))
+  (log/debug :dispatch (str "Unsubscribing from " (data/get-jam-teleporters)))
+  ;;(.unsubscribe mqtt-manager [(data/get-jam-teleporters)])
+  (.unsubscribe mqtt-manager ["foobar"])
+  #_#_#_(.publish mqtt-manager (data/get-jam) {:message/type :jam.cmd/ended
+                                         :message/body {:teleporter/id (data/get-tp-id)
+                                                        :jam/uuid (data/get-jam-id)}})
+  (data/clear-jam-id!)
+  (data/clear-jam-session!)
+  
+
+  )
