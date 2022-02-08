6d32ed7b62c16d88aced98e573c1079a52d685af
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2022-02-08 13:11:25 +0100

    Add utils to check the current installed version of an apt package and a function to check for/delete upgrading_flag file

--- tpx-clj/src/tpx/utils.clj
+++ tpx-clj/src/tpx/utils.clj
@@ -1,4 +1,8 @@
-(ns tpx.utils)
+(ns tpx.utils
+  (:require [clojure.string :as str]
+            [clojure.java.shell :refer [sh]]
+            [taoensso.timbre :as log]
+            [clojure.java.io :as io]))
 
 (defn scale-value
   "Linearly transforms x from range input-range to output-range where:
@@ -19,3 +23,25 @@
      (-> (- x a)
          (/ (- b a))
          (* d)))))
+
+(defn get-apt-package-installed-version
+  "Returns the version of an installed apt package.
+  Returns nil if the package is not installed"
+  [package-name]
+  (let [apt-show-command (str "apt-cache policy " package-name)
+        apt-string (:out (sh "bash" "-c" apt-show-command))
+        version-line (first (filter (fn [line] (re-matches #"  Installed.*" line))
+                                    (str/split apt-string #"\r\n")))
+        version (last (str/split version-line #": "))]
+    (if-not (= version "(none)")
+      version
+      nil)))
+
+(defn upgrading-flag?
+  "Checks for the upgrading_flag file"
+  []
+  (.exists (io/file "upgrading_flag")))
+
+(defn delete-upgrading-flag []
+  (log/debug ::delete-upgrading-flag "deleting flag")
+  (io/delete-file "upgrading_flag"))
