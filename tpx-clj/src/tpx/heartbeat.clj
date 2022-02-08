45c66c72722e7d6c67e8615a6373426b6cbcf081
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2022-02-08 13:07:33 +0100

    Use chime for scheduling heartbeat, fix send-message! in heartbeat

--- tpx-clj/src/tpx/heartbeat.clj
+++ tpx-clj/src/tpx/heartbeat.clj
@@ -2,32 +2,37 @@
   "Heartbeat sender, send a hearbeat periodically on mqtt"
   (:require [com.stuartsierra.component :as component]
             [taoensso.timbre :as log]
-            [tpx.data :as data]))
+            [tpx.data :as data]
+            [chime.core :as chime])
+  (:import (java.time Instant Duration)))
 
 (defonce ^:private store (atom nil))
 (defn send-message! [msg]
-  (let [heartbeat @store
-        injections (-> heartbeat
-                       (select-keys (:injection-ks heartbeat))
-                       (assoc :heartbeat heartbeat))]
+  (let [mqtt-manager (:mqtt-manager @store)
+        injections (-> mqtt-manager
+                       (select-keys (:injection-ks mqtt-manager))
+                       (assoc :mqtt-manager mqtt-manager))]
     (.send-message! (:message-service injections) (merge msg injections))))
 
 (defn set-interval [callback ms]
-  (future (while true (do (Thread/sleep ms) (callback)))))
+  (chime/chime-at (chime/periodic-seq (Instant/now) (Duration/ofMillis ms)) (fn[time] (callback time)))
+  #_(future (while true (do (Thread/sleep ms) (callback)))))
 
 (defn send-heartbeat []
   (log/debug ::send-heartbeat "sending heartbeat")
   (send-message! {:message/type :teleporter.cmd/send-heartbeat}))
 
-(defrecord HeartbeatService [injection-ks started? config message-service]
+(defrecord HeartbeatService [injection-ks started? config message-service mqtt-manager]
   component/Lifecycle
   (start [this]
     (if started?
       this
       (do (log/info "Starting HeartbeatService")
-          (let [new-this (assoc this
+          (let [timer (get config :timer (* 60 1000))
+                new-this (assoc this
+                                :mqtt-manager mqtt-manager
                                 :started? true
-                                :job (set-interval #(send-heartbeat) (get config :timer (* 60 1000))))]
+                                :job (set-interval (fn[_] (send-heartbeat)) timer))]
             (reset! store new-this)
             new-this))))
   (stop [this]
@@ -36,7 +41,7 @@
       (do (log/info "Stopping HeartbeatService")
           (let [new-this (assoc this
                                 :started? false)]
-            (future-cancel (:job this))
+            (.close (:job this))
             (reset! store nil)
             new-this)))))
 
