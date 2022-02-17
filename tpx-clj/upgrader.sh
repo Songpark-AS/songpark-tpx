ef1318459ff68b0c214574ab6bfb4b5a9cceb80f
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2022-02-17 14:47:58 +0100

    Only set upgrade flag and reboot after the upgrade command has been run successfully

--- tpx-clj/upgrader.sh
+++ tpx-clj/upgrader.sh
@@ -5,15 +5,17 @@ output_dir=$1
 echo "stopping services"
 systemctl stop sp-tpx sp-bridgeprogram
 
-# set upgrading flag that'll be picked up by TPX on next boot
-echo "Setting upgrading_flag"
-echo "upgrading" > "$output_dir"/upgrading_flag
-
 apt-get update
 
 # perform the upgrade
 echo "upgrading"
 apt-get install -y --only-upgrade teleporter-fw
+status=$?
+if $(exit $status); then
+    # set upgrading flag that'll be picked up by TPX on next boot
+    echo "Setting upgrading_flag"
+    echo "upgrading" > "$output_dir"/upgrading_flag
 
-echo "rebooting"
-reboot
+    echo "rebooting"
+    reboot
+fi
