9a4bacf4e23c4ae093d41510fa2f5b901a1a146c
Emil Bengtsson <emil@emil0r.com>
2022-09-11 14:59:38 +0200

    Remove need for a button on the Teleporter to pair with it
    This is done for demo purposes. It's only been commented out.

--- tpx-clj/src/tpx/mqtt/handler/pairing.clj
+++ tpx-clj/src/tpx/mqtt/handler/pairing.clj
@@ -1,14 +1,25 @@
 (ns tpx.mqtt.handler.pairing
-  (:require [taoensso.timbre :as log]
+  (:require [songpark.common.communication :refer [POST]]
+            [taoensso.timbre :as log]
             [tpx.data :as data]
             [tpx.pairing :as pairing]
+            [tpx.utils :as util]
             [songpark.mqtt :as mqtt :refer [handle-message]]))
 
 (defmethod handle-message :pairing/pair [{id :teleporter/id
                                           user-id :auth.user/id
                                           gpio :gpio}]
-  (pairing/set-status gpio :pairing)
-  (data/set-user-id! user-id))
+  ;; (pairing/set-status gpio :pairing)
+  (let [platform-url (util/get-platform-url "/api/teleporter/pair")]
+    (log/debug "Setting to paired")
+
+    (data/set-user-id! user-id)
+    (POST platform-url
+          {:message/type :pairing/paired
+           :teleporter/id (data/get-tp-id)
+           :auth.user/id (data/get-user-id)}
+          (fn [_]
+            (pairing/set-status gpio :paired)))))
 
 
 (defmethod handle-message :pairing/unpair [{id :teleporter/id
